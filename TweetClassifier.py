832030c947ae101811504f46c617e8e1d3021e66
Paulo Henrique <paulostation0@gmail.com>
2018-04-01 16:45:32 -0300

    Added neutral class, classifier still doesn't give accurate results
    Signed-off-by: Paulo Henrique <paulostation0@gmail.com>

--- TweetClassifier.py
+++ TweetClassifier.py
@@ -19,30 +19,36 @@ def word_feats(words):
 def classify(text):
     features = word_feats(text.split())
     classification = classifier.classify(features)
-    if classification == "pos":
-        return "positive"
-    else:
-        return "negative"
+    return classification
 
-df = pd.read_csv('datasets/labeled_tweets.csv', names=['sentiment', 'text']);
+df = pd.read_csv('datasets/labeled_tweets.csv', names=['text', 'sentiment']);
 
 # separate positive tweets only
 positive_tweets = df.loc[df['sentiment'] == 'positive']['text'].tolist()
 # separate negative tweets only
 negative_tweets = df.loc[df['sentiment'] == 'negative']['text'].tolist()
+# separate neutral tweets only
+neutral_tweets = df.loc[df['sentiment'] == 'neutral']['text'].tolist()
 
 positive_tweets = map(filter_non_ascii, positive_tweets)
 negative_tweets = map(filter_non_ascii, negative_tweets)
+neutral_tweets = map(filter_non_ascii, negative_tweets)
 
+positive_tweets = positive_tweets[:69]
+negative_tweets = negative_tweets[:69]
+neutral_tweets = neutral_tweets[:69]
 
-posfeats = [(word_feats(tweet.split()), 'pos') for tweet in positive_tweets]
-negfeats = [(word_feats(tweet.split()), 'neg') for tweet in negative_tweets]
+posfeats = [(word_feats(tweet.split()), 'positive') for tweet in positive_tweets]
+negfeats = [(word_feats(tweet.split()), 'negative') for tweet in negative_tweets]
+neutralfeats = [(word_feats(tweet.split()), 'neutral') for tweet in neutral_tweets]
+
 # separate 75% for train, 25% for test
 negcutoff = len(negfeats)*3/4
 poscutoff = len(posfeats)*3/4
+neutralcutoff = len(neutralfeats)*3/4
 
-trainfeats = negfeats[:negcutoff] + posfeats[:poscutoff]
-testfeats = negfeats[negcutoff:] + posfeats[poscutoff:]
+trainfeats = negfeats[:negcutoff] + posfeats[:poscutoff] + neutralfeats[:neutralcutoff]
+testfeats = negfeats[negcutoff:] + posfeats[poscutoff:] + neutralfeats[neutralcutoff:]
 print 'train on %d instances, test on %d instances' % (
     len(trainfeats), len(testfeats))
 
